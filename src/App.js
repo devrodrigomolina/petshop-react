1ceead68cc709024c4fbe816280349a39bd637d8
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-03 04:31:58 -0300

    Adicionado rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
-
+import Home from "./Components/Pages/Home";
+import './App.css'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
