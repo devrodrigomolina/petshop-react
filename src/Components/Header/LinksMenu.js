dae86f2346c6e290411ff6a815bc0272bdac7de9
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-04 01:46:41 -0300

    Organização do codigo

--- src/Components/Header/LinksMenu.js
+++ src/Components/Header/LinksMenu.js
@@ -6,9 +6,7 @@ import style from "./LinksMenu.module.css";
 const HeaderLinks = () => {
   return (
     <ul className={style.ul}>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
+      <NavLink exact to="/">Home</NavLink>
       <NavLink to="sobre">Sobre</NavLink>
       <NavLink to="loja">Loja</NavLink>
       <NavLink to="contato">Contato</NavLink>
