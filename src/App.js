2cee0527dc1fc9194c435b525ae9831a68ef11d8
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-05 00:03:33 -0300

    Criado novas rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import Home from "./Components/Pages/Home";
+import Home from "./Components/Pages/Home/Home";
 import './App.css'
 import Footer from "./Components/Footer/Footer";
+import Sobre from "./Components/Pages/Sobre/Sobre";
+import Loja from "./Components/Pages/Loja/Loja";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sobre" element={<Sobre />}></Route>
+        <Route path="/loja" element={<Loja />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
