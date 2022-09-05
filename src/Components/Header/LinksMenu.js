932944ccc120580b929590e86fdf732ce6a57271
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-05 00:04:32 -0300

    Removido icone de cart

--- src/Components/Header/LinksMenu.js
+++ src/Components/Header/LinksMenu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { IoMdCart } from 'react-icons/io'
+import Cart from "../Cart/Cart";
 import style from "./LinksMenu.module.css";
 
 const HeaderLinks = () => {
@@ -10,9 +10,7 @@ const HeaderLinks = () => {
       <NavLink to="sobre">Sobre</NavLink>
       <NavLink to="loja">Loja</NavLink>
       <NavLink to="contato">Contato</NavLink>
-      <NavLink to="cart">
-        <IoMdCart style={{fontSize: '1.6rem', color: 'white'}} />
-      </NavLink>
+      <Cart />
     </ul>
   );
 };
