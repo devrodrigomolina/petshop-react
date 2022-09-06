ece478f9afa9348256e2a5adcdc2f939507e86bd
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-06 00:35:12 -0300

    Adicionado descrição dos produtos

--- src/Components/Produtos/JsonProdutos.js
+++ src/Components/Produtos/JsonProdutos.js
@@ -1,87 +1,106 @@
-import produto1 from '../Assets/Produtos/1.png'
-import produto2 from '../Assets/Produtos/2.png'
-import produto3 from '../Assets/Produtos/3.png'
-import produto4 from '../Assets/Produtos/4.png'
-import produto5 from '../Assets/Produtos/5.png'
-import produto6 from '../Assets/Produtos/6.png'
-import produto7 from '../Assets/Produtos/7.png'
-import produto8 from '../Assets/Produtos/8.png'
-import produto9 from '../Assets/Produtos/9.png'
-import produto10 from '../Assets/Produtos/10.png'
-import produto11 from '../Assets/Produtos/11.png'
-import produto12 from '../Assets/Produtos/12.png'
+import produto1 from "../Assets/Produtos/1.png";
+import produto2 from "../Assets/Produtos/2.png";
+import produto3 from "../Assets/Produtos/3.png";
+import produto4 from "../Assets/Produtos/4.png";
+import produto5 from "../Assets/Produtos/5.png";
+import produto6 from "../Assets/Produtos/6.png";
+import produto7 from "../Assets/Produtos/7.png";
+import produto8 from "../Assets/Produtos/8.png";
+import produto9 from "../Assets/Produtos/9.png";
+import produto10 from "../Assets/Produtos/10.png";
+import produto11 from "../Assets/Produtos/11.png";
+import produto12 from "../Assets/Produtos/12.png";
 
 export const produtos = [
   {
     id: 0,
     src: produto1,
-    text: 'Ração Golden para Gatos Adultos Sabor Frango',
-    price: 'R$ 36,00'
+    text: "Ração Golden para Gatos Adultos Sabor Frango",
+    desc: [
+      "Indicada para gatos",
+      "Contém fórmula específica para felinos castrados",
+      "Auxilia no controle de peso",
+      "Proporciona saúde ao trato urinário",
+      "Livre de corantes e aromatizantes artificiais",
+    ],
+    price: "R$ 36,00",
   },
   {
     id: 1,
     src: produto2,
-    text: 'Antipulgas e Carrapatos Bravecto MSD para Cães',
-    price: 'R$ 245,00'
+    text: "Antipulgas e Carrapatos Bravecto MSD para Cães",
+    desc: [
+      "Indicado para cães",
+      "Acaba com a infestação de carrapatos e pulgas",
+      "Rápida ação",
+      "Efeito prolongado por 12 semanas",
+    ],
+    price: "R$ 245,00",
   },
   {
     id: 2,
     src: produto3,
-    text: 'Ração Mix de Carnes para Gatos Adultos',
-    price: 'R$ 56,00'
+    text: "Ração Mix de Carnes para Gatos Adultos",
+    desc: [
+      "Indicada para gatos adultos",
+      "100% completa e balanceada",
+      "Fórmula livre de corantes artificiais",
+      "Minerais controlados para ajudar a manter o trato urinário saudável.",
+    ],
+    price: "R$ 56,00",
   },
   {
     id: 3,
     src: produto4,
-    text: 'Ração Golden Special Sabor Frango e Carne para Cães Adultos',
-    price: 'R$ 136,00'
+    text: "Ração Golden Special Sabor Frango e Carne para Cães Adultos",
+    price: "R$ 136,00",
   },
   {
     id: 4,
     src: produto5,
-    text: 'Ração Royal Canin Boxer Puppy',
-    price: 'R$ 236,00'
+    text: "Ração Royal Canin Boxer Puppy",
+    price: "R$ 236,00",
   },
   {
     id: 5,
     src: produto6,
-    text: 'Ração para Cães Filhotes Sabor Frango',
-    price: 'R$ 23,00'
+    text: "Ração para Cães Filhotes Sabor Frango",
+    price: "R$ 23,00",
   },
   {
     id: 6,
     src: produto7,
-    text: 'ADADAD',
-    price: 'R$ 23,00'
+    text: "ADADAD",
+    price: "R$ 23,00",
   },
   {
     id: 7,
     src: produto8,
-    text: '12121212or Frango',
-    price: 'R$ 23,00'
+    text: "12121212or Frango",
+    price: "R$ 23,00",
   },
   {
     id: 8,
     src: produto9,
-    text: 'Raadasdasdtes S',
-    price: 'R$ 23,00'
+    text: "Raadasdasdtes S",
+    price: "R$ 23,00",
   },
   {
     id: 9,
     src: produto10,
-    text: 'smksmgn ngo',
-    price: 'R$ 23,00'
+    text: "smksmgn ngo",
+    price: "R$ 23,00",
   },
   {
     id: 10,
     src: produto11,
-    text: 'oçioiouio',
-    price: 'R$ 23,00'
+    text: "oçioiouio",
+    price: "R$ 23,00",
   },
   {
     id: 11,
     src: produto12,
-    text: 'mbmbmbnmbnmbngo',
-    price: 'R$ 23,00'
+    text: "mbmbmbnmbnmbngo",
+    price: "R$ 23,00",
   },
-]
+];
