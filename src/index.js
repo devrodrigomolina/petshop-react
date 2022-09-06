683ccfcee12df64401d461956f5313f9d42e1598
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-06 00:35:58 -0300

    Removido modo estrito

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
 
