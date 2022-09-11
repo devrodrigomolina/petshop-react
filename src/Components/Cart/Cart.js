c3b13412871cff92aad58cf0fa53a4d2a9cafac4
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-11 03:11:07 -0300

    Adicionado funcionalidade de abrir e fechar o modal

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { IoMdCart } from "react-icons/io";
+import ModalCart from "../Modal/ModalCart";
 import style from "./Cart.module.css";
-const Cart = () => {
-  const [itenscart, setItenscart] = useState(3);
 
-  const teste = () => {
-    alert("VOCE FOI TESTADO");
+const Cart = () => {
+  const [itenscart, setItenscart] = useState(0);
+  const [modalCart, setModalCart] = useState(false);
+
+  const openModalCart = () => {
+    setModalCart(!modalCart);
   };
+
   return (
     <div className={style.cart_container}>
       {itenscart > 0 && <span className={style.cart_total}>{itenscart}</span>}
-      <IoMdCart onClick={teste} className={style.cart} />
+      <IoMdCart onClick={openModalCart} className={style.cart} />
+      <ModalCart modalCart={modalCart} />
     </div>
   );
 };
