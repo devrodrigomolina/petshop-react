25086c2f33b552ef52392629f780971122ef866d
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-11 03:11:54 -0300

    adicionado propriedade width para ajustar tamanho do botao

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import style from './Button.module.css'
-const Button = ({ text, background, color }) => {
-  return <button style={{background: `${background}`, color: `${color}`}} className={style.btn}>{text}</button>
-}
+import React from "react";
+import style from "./Button.module.css";
+const Button = ({ text, background, color, width, onClick }) => {
+  return (
+    <button
+      style={{ background: `${background}`, color: `${color}`, width: `${width}` }}
+      className={style.btn}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
