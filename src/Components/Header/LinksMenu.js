31cccec910e54aad3698cae5c9a7c9a51859c8d5
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-09-11 03:10:46 -0300

    Envolvido componente Cart em uma div

--- src/Components/Header/LinksMenu.js
+++ src/Components/Header/LinksMenu.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Cart from "../Cart/Cart";
+
 import style from "./LinksMenu.module.css";
 
 const HeaderLinks = () => {
   return (
     <ul className={style.ul}>
-      <NavLink exact to="/">Home</NavLink>
+      <NavLink exact='true' to="/">Home</NavLink>
       <NavLink to="sobre">Sobre</NavLink>
       <NavLink to="loja">Loja</NavLink>
       <NavLink to="contato">Contato</NavLink>
-      <Cart />
+      <div>
+        <Cart  />
+      </div>
     </ul>
   );
 };
