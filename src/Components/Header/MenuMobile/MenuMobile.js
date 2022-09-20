c75cdba18d7c29d54f487ffa3d94711aaf140c52
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-19 23:50:50 -0300

    Implementação do menu mobile

--- src/Components/Header/MenuMobile/MenuMobile.js
+++ src/Components/Header/MenuMobile/MenuMobile.js
@@ -1,23 +1,31 @@
-import React from "react";
-import style from './MenuMobile.module.css'
-import LinksMenu from '../LinksMenu'
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import style from "./MenuMobile.module.css";
+import LinksMenu from "../LinksMenu";
+import Logo from "../../Logo/Logo";
+
 const MenuMobile = () => {
+  const [checkbox, setCheckbox] = useState(false);
+
   return (
     <div className={style.main_container}>
-      <input type="checkbox" id={style.bgtoggle} />
-      <div className={style.card}>
-        <label for={style.bgtoggle} className={style.menu}></label>
-        <div className={style.menu_container}>
-          <div className={style.links_menu}>
-            <LinksMenu />
-          </div>
+      <input
+        type="checkbox"
+        checked={checkbox}
+        onChange={({ target }) => setCheckbox(target.checked)}
+        id={style.active}
+      />
+      <label for={style.active} className={style.menu_btn}>
+        <div className={style.menu}>
+          <div id={style.bar} className={checkbox && style.animate}></div>
         </div>
+      </label>
+      <div className={style.wrapper}>
+        <Logo width={'100px'} />
+        <LinksMenu />
       </div>
     </div>
   );
 };
 
-/* https://codepen.io/HAbdellah/pen/YzNqKPJ */
 
 export default MenuMobile;
